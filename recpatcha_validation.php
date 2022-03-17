7e6114fa2024550762e1e9e5ce7975979de2e5a7
EMMANUEL ATSU <atsuemmanuel@gmail.com>
2022-03-17 18:35:37 +0100

    Create recpatcha_validation.php

---
+++ recpatcha_validation.php
@@ -0,0 +1,29 @@
+<?php
+
+///////////////// GOOGLE RECAPTCHA V3 (INVISIBLE) STARTS ///////////////// 
+	$secretKey 	= 'SECRET_KEY_HERE';
+	$token 		= $_POST["g-token"];
+	$ip			= $_SERVER['REMOTE_ADDR'];
+	
+	$url = "https://www.google.com/recaptcha/api/siteverify";
+	$data = array('secret' => $secretKey, 'response' => $token, 'remoteip'=> $ip);
+ 
+	// use key 'http' even if you send the request to https://...
+	$options = array('http' => array(
+		'method'  => 'POST',
+		'content' => http_build_query($data)
+	));
+	$context  = stream_context_create($options);
+	$result = file_get_contents($url, false, $context);
+	$response = json_decode($result);
+	if($response->success)
+	{
+		//echo '<center><h1>Validation Success!</h1></center>';
+    //---YOUR ACTION CONTINUES HERE---//
+	}
+	else
+	{
+		//echo '<center><h1>Captcha Validation Failed..!</h1></center>';exit;
+		echo '<script>window.location.href = "https://your-web-site.com/contact";</script>';exit;
+	}
+///////////////// GOOGLE RECAPTCHA V3 (INVISIBLE) ENDS ///////////////// 
