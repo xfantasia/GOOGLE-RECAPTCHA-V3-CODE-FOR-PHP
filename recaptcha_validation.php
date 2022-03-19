5e82620abfcd6e1ef8cbb2ea6f5db7d7f3f224c0
EMMANUEL ATSU <atsuemmanuel@gmail.com>
2022-03-19 23:40:43 +0100

    Update recaptcha_validation.php

--- recaptcha_validation.php
+++ recaptcha_validation.php
@@ -2,8 +2,9 @@
 
 ///////////////// GOOGLE RECAPTCHA V3 (INVISIBLE) STARTS ///////////////// 
 	$secretKey 	= 'SECRET_KEY_HERE';
+	$page_url 	= 'SUCCESS_URL_HERE';
 	$token 		= $_POST["g-token"];
-	$ip			= $_SERVER['REMOTE_ADDR'];
+	$ip		= $_SERVER['REMOTE_ADDR'];
 	
 	$url = "https://www.google.com/recaptcha/api/siteverify";
 	$data = array('secret' => $secretKey, 'response' => $token, 'remoteip'=> $ip);
@@ -19,11 +20,11 @@
 	if($response->success)
 	{
 		//echo '<center><h1>Validation Success!</h1></center>';
-    //---YOUR ACTION CONTINUES HERE---//
+    		//---YOUR ACTION CONTINUES HERE---//
 	}
 	else
 	{
 		//echo '<center><h1>Captcha Validation Failed..!</h1></center>';exit;
-		echo '<script>window.location.href = "https://your-web-site.com/contact";</script>';exit;
+		echo '<script>window.location.href = $page_url;</script>';exit;
 	}
 ///////////////// GOOGLE RECAPTCHA V3 (INVISIBLE) ENDS ///////////////// 
